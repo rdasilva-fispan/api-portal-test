2d86c67044245da96c2944eef9cdab33c703ab4e
Rodrigo da Silva <rdasilva@fispan.com>
2021-07-23 10:21:58 -0300

    Trying againg

--- openapi/api-docs/plugins/allow-bank-plugin.js
+++ openapi/api-docs/plugins/allow-bank-plugin.js
@@ -1,10 +1,19 @@
-const allowBank = require('./decorators/allow-bank');
 const id = 'allow-bank';
 
 /** @type {import('@redocly/openapi-cli').CustomRulesConfig} */
 const decorators = {
     oas3: {
-        'allow-path-for-bank': allowBank
+        'allow-path-for-bank': () => {
+            return {
+                PathItem: {
+                    leave(pathItem, ctx) {
+                        if (!pathItem[process.env.ALLOW_BANK_FLAG_NAME]) {
+                            delete ctx.parent[ctx.key];
+                        }
+                    }
+                }
+            }
+        }
     }
 };
 
