cf045f0978ff88318115ed580b15351dae5c7845
Rodrigo da Silva <rdasilva@fispan.com>
2021-07-23 10:48:08 -0300

    Changing allow to disallow

--- openapi/api-docs/plugins/disallow-bank-plugin.js
+++ openapi/api-docs/plugins/disallow-bank-plugin.js
@@ -1,13 +1,13 @@
-const id = 'allow-bank';
+const id = 'disallow-bank';
 
 /** @type {import('@redocly/openapi-cli').CustomRulesConfig} */
 const decorators = {
     oas3: {
-        'allow-path-for-bank': () => {
+        'disallow-path-for-bank': () => {
             return {
                 PathItem: {
                     leave(pathItem, ctx) {
-                        if (pathItem[process.env.ALLOW_BANK_FLAG_NAME]) {
+                        if (pathItem[process.env.DISALLOW_BANK_FLAG_NAME]) {
                             delete ctx.parent[ctx.key];
                         }
                     }
