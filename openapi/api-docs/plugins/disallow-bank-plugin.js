79462cf4b586ca607abd2e1b077808507f9ee6a9
Rodrigo da Silva <rdasilva@fispan.com>
2021-07-26 15:17:52 -0300

    Trying to remove Unused Tags.

--- openapi/api-docs/plugins/disallow-bank-plugin.js
+++ openapi/api-docs/plugins/disallow-bank-plugin.js
@@ -4,26 +4,41 @@ const id = 'disallow-bank';
 const decorators = {
     oas3: {
         'disallow-path-for-bank': () => {
+            const tagsToRemove = new Set();
+
             return {
                 PathItem: {
                     leave(pathItem, ctx) {
+                        let deletePath = false;
+
                         if (pathItem[process.env.DISALLOW_BANK_FLAG_NAME]) {
                             console.log("Deleting " + ctx.parent[ctx.key]);
                             delete ctx.parent[ctx.key];
+                            deletePath = true;
                         }
 
                         // delete any operations inside of a path marked with x-internal
-                        // const operations = ['get', 'put', 'post', 'delete', 'options', 'head', 'patch', 'trace'];
-                        // for (const operation of operations) {
-                        //     if (pathItem[operation] && pathItem[operation][process.env.DISALLOW_BANK_FLAG_NAME]) {
-                        //         delete pathItem[operation];
-                        //     }
-                        // }
+                        const operations = ['get', 'put', 'post', 'delete', 'options', 'head', 'patch', 'trace'];
+                        for (const operation of operations) {
+                            if (pathItem[operation] && pathItem[operation][process.env.DISALLOW_BANK_FLAG_NAME]) {
+                                delete pathItem[operation];
+                            }
+                            if (deletePath) {
+                                tagsToRemove.add(pathItem[operation].tags || []);
+                            }
+                        }
 
-                        // // delete the path if there are no operations remaining in it
-                        // if (Object.keys(pathItem).length === 0) {
-                        //     delete ctx.parent[ctx.key];
-                        // }
+                        // delete the path if there are no operations remaining in it
+                        if (Object.keys(pathItem).length === 0) {
+                            delete ctx.parent[ctx.key];
+                            deletePath = true;
+                        }
+                    },
+                },
+
+                DefinitionRoot: {
+                    leave(root) {
+                        root.tags = root.tags.filter((tag) => tagsToRemove.has(tag.name));
                     }
                 }
             }
