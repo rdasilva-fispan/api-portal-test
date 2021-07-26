4180e2eb797df1c17378707f6f7a76ce60b5544d
Rodrigo da Silva <rdasilva@fispan.com>
2021-07-26 10:51:09 -0300

    Changing it again.

--- openapi/api-docs/plugins/disallow-bank-plugin.js
+++ openapi/api-docs/plugins/disallow-bank-plugin.js
@@ -12,17 +12,17 @@ const decorators = {
                         }
 
                         // delete any operations inside of a path marked with x-internal
-                        const operations = ['get', 'put', 'post', 'delete', 'options', 'head', 'patch', 'trace'];
-                        for (const operation of operations) {
-                            if (pathItem[operation] && pathItem[operation][process.env.DISALLOW_BANK_FLAG_NAME]) {
-                                delete pathItem[operation];
-                            }
-                        }
+                        // const operations = ['get', 'put', 'post', 'delete', 'options', 'head', 'patch', 'trace'];
+                        // for (const operation of operations) {
+                        //     if (pathItem[operation] && pathItem[operation][process.env.DISALLOW_BANK_FLAG_NAME]) {
+                        //         delete pathItem[operation];
+                        //     }
+                        // }
 
-                        // delete the path if there are no operations remaining in it
-                        if (Object.keys(pathItem).length === 0) {
-                            delete ctx.parent[ctx.key];
-                        }
+                        // // delete the path if there are no operations remaining in it
+                        // if (Object.keys(pathItem).length === 0) {
+                        //     delete ctx.parent[ctx.key];
+                        // }
                     }
                 }
             }
