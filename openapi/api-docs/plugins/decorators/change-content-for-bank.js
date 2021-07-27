ed652b3560c5fe1129455cc3a4b5fd51fcb9754f
Rodrigo da Silva <rdasilva@fispan.com>
2021-07-27 10:54:08 -0300

    Trying to fix ref

--- openapi/api-docs/plugins/decorators/change-content-for-bank.js
+++ openapi/api-docs/plugins/decorators/change-content-for-bank.js
@@ -5,9 +5,9 @@ function ChangeContentForBank() {
     return {
         DefinitionRoot: {
             leave(root) {
-                root['info']['title'] = process.env.INFO_TITLE;
-                root['info']['x-logo']['url'] = process.env.INFO_XLOGO_URL;
-                root['info']['description']['$ref'] = process.env.INFO_DESCRIPTION_REF;
+                root['info']['title'] = `${process.env.INFO_TITLE}`;
+                root['info']['x-logo']['url'] = `${process.env.INFO_XLOGO_URL}`;
+                root['info']['description']['$ref'] = `${process.env.INFO_DESCRIPTION_REF}`;
             },
         }
     }
