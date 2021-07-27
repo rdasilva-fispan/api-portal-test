8b18395c5f4726209f0dd062df1d7d69a09df811
Rodrigo da Silva <rdasilva@fispan.com>
2021-07-27 11:02:38 -0300

    Printing logs.

--- openapi/api-docs/plugins/decorators/change-content-for-bank.js
+++ openapi/api-docs/plugins/decorators/change-content-for-bank.js
@@ -5,6 +5,9 @@ function ChangeContentForBank() {
     return {
         DefinitionRoot: {
             leave(root) {
+                console.log(`INFO_TITLE           = ${process.env.INFO_TITLE}`);
+                console.log(`INFO_XLOGO_URL       = ${process.env.INFO_XLOGO_URL}`);
+                console.log(`INFO_DESCRIPTION_REF = ${process.env.INFO_DESCRIPTION_REF}`);
                 root['info']['title'] = `${process.env.INFO_TITLE}`;
                 root['info']['x-logo']['url'] = `${process.env.INFO_XLOGO_URL}`;
                 root['info']['description']['$ref'] = `${process.env.INFO_DESCRIPTION_REF}`;
